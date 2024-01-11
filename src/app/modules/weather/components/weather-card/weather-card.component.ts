63bd6f7f45d162a94e25edd97a49f4a21b735562
Jean Carlos Galhardi <jeancarlosgalhardi@gmail.com>
2024-01-11 09:28:51 -0300

    Projeto Pronto

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,18 @@
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherDatas } from './../../../../models/interfaces/WeatherDatas';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+
+export class WeatherCardComponent {
+  @Input()WeatherDatasInput!: WeatherDatas;
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperaturaIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+}
